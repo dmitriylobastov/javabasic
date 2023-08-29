24b7be60c8f9ce817f7ca1048ffd38ad235de99e
dmitriylobastov <119615202+dmitriylobastov@users.noreply.github.com>
2023-08-29 23:36:07 +0300

    Create operators.java
    New class operators created

---
+++ src/main/java/javabasic/operators.java
@@ -0,0 +1,17 @@
+package javabasic;
+
+import java.util.Scanner;
+
+public class operators {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        int z = scan.nextInt();
+        double average = (x + y + z) / 3.0;
+        System.out.println(average);
+        int result = (int) Math.floor(average / 2);
+        if (result > 3)
+            System.out.println("Программа выполнена корректно");
+    }
+}
