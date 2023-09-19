4d17fe18689f1d15739957e297885e5db95b5833
dmitriylobastov <119615202+dmitriylobastov@users.noreply.github.com>
2023-09-19 20:29:28 +0300

    Update finaltask4.java
    Ready

--- src/main/java/javabasic/finaltask4.java
+++ src/main/java/javabasic/finaltask4.java
@@ -5,24 +5,38 @@ import java.util.Scanner;
 public class finaltask4 {
     public static void main(String[] args) {
         System.out.println("Отгадайте загадку: “Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает”.");
-        System.out.println("- Есть 3 попытки, чтобы отгадать загадку");
-        System.out.println("- Доступна подсказка перед первой попыткой, но доступных попыток вместо трех станет одна");
-        System.out.println("- Чтобы взять подсказку, на первой попытке введите: Подсказка");
-        System.out.println("Первая попытка: ");
-        String input = input();
-        String [] array = {"Подсказка", "Правильно!", "Подумай еще!", "Обидно, приходи в другой раз", "Подсказка уже недоступна", "Какой-то жук, найденный в архиве", "У Вас единственная попытка: ", "Заархивированный вирус"};
-        // удалить строку ->
-        System.out.println(input);
-        if (input.toLowerCase().contains((array[0]).toLowerCase())) {
-            System.out.println(array[0] + ": " + array[5]);
-            System.out.println(array[6]);
+        System.out.println("- есть 3 попытки, чтобы отгадать загадку");
+        System.out.println("- доступна подсказка перед первой попыткой, но доступных попыток вместо трех станет одна");
+        System.out.println("- чтобы взять подсказку, на первой попытке введите: Подсказка");
+        String input;
+        int counter = 3;
+        String[] array = {"Подсказка", "Правильно!", "Подумай еще!", "Обидно, приходи в другой раз", "Подсказка уже недоступна", ": Какой-то жук, найденный в архиве", "Заархивированный вирус"};
+        while (counter > 0) {
+            System.out.println("Осталось попыток: " + counter + ". Введите ответ: ");
             input = input();
-            if (input.toLowerCase().contains((array[7]).toLowerCase())) {
-                System.out.println(array[1]);
+            if (input.toLowerCase().contains((array[0]).toLowerCase())) {
+                if (counter == 3) {
+                    System.out.println(array[0] + array[5]);
+                    counter = 1;
+                } else {
+                    System.out.println(array[4]);
+                }
+                continue;
             }
+            if (input.toLowerCase().contains((array[6]).toLowerCase())) {
+                System.out.println(array[1]);
+                break;
+            }
+            if (!input.toLowerCase().contains((array[6]).toLowerCase())) {
+                counter--;
+                if (counter > 0) {
+                    System.out.println(array[2]);
+                    continue;
+                }
+            }
+            System.out.println(array[3]);
         }
     }
-
 
     public static String input() {
         Scanner scan = new Scanner(System.in);
@@ -34,7 +48,6 @@ public class finaltask4 {
             }
             return input;
         }
-            System.out.println("Введите: I like Java!!!");
         return input;
     }
 }
